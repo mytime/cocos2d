c2c0138aeed7a0fc2a4ae91bbccf4eb035f698f2
mytime <cmba@qq.com>
2016-04-25 16:06:56 +0800

    03

---
+++ 读取XML.cpp
@@ -0,0 +1,63 @@
+#include "HelloWorldScene.h"
+
+//解析XML文件头
+#include <tinyxml2/tinyxml2.h>
+
+USING_NS_CC;
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+   //static Director* getInstance() 返回一个导演单例   
+    Size visibleSize = Director::getInstance()->getVisibleSize(); //返回OpenGL视图的大小，以点为单位
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	////////////////////////////////
+	//XML文件读取
+
+	auto doc = new tinyxml2::XMLDocument();
+	doc->Parse(FileUtils::getInstance()->getStringFromFile("data.xml").c_str());
+	auto root = doc->RootElement();
+	for (auto e = root->FirstChildElement(); e; e=e->NextSiblingElement()){
+		std::string str;
+		for (auto attr = e->FirstAttribute(); attr; attr=attr->Next()){
+			str+=attr->Name();
+			str+=":";
+			str+=attr->Value();
+			str+=",";
+		}
+		log("%s",str.c_str());		
+	}	
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
